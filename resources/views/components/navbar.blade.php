53012e879b1b9cf95c9e99ff0900c9913db06213
Vito Basile <82734808+DarienB11@users.noreply.github.com>
2023-06-15 17:19:33 +0200

    implementati login e register

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,17 +1,23 @@
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Presto</a>
+        {{-- INSERIRE LOGO --}}
+        <a class="navbar-brand" href="{{ route('home')}}">Presto</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                </li>
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register')}}">Registrati</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="{{ route('login')}}">Login</a>
                 </li>
+                @else
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
@@ -22,14 +28,16 @@
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="#">Something else here</a></li>
                     </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
+                </li> 
+                <form action="{{route('logout')}}" method="post">
+                    @csrf
+                    <button class="btn btn-danger">Logout</button>
+                </form>
+                @endguest       
             </ul>
             <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <button class="btn btn-outline-success" type="submit">Cerca</button>
             </form>
         </div>
     </div>
