5b8ac525a813649be8dcc13cf8f6fb65ab44c4bb
salvatore bonomo <salvatore.bonomo20@gmail.com>
2023-06-21 21:15:56 +0200

    risoluzione problemi merge

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid textCustom">
       <h1 class="display-5 fw-bold">Presto Ecommerce</h1>
       <p class="col-md-8 fs-4">
-        "Scopri il piacere dello shopping sostenibile nel nostro ecommerce di prodotti usati. Risparmia denaro, riduci gli sprechi. Dai valore ai tuoi soldi, trova tesori nascosti a prezzi incredibili<br>
+        "Scopri il piacere dello shopping sostenibile nel nostro ecommerce di prodotti usati. Risparmia denaro, riduci gli sprechi. Dai valore ai tuoi soldi, trova tesori nascosti a prezzi incredibili"<br>
         <a class="btn btn-danger btn-lg mt-3" href="{{route('announcement.index')}}">Vai allo shop</a>
     </div>
 </div>
