af07a6d9b6704d4ae4621c75bc57d452a1b4920c
Giammarco De Giorgi <giammarco.degiorgi@gmail.com>
2023-06-27 19:24:39 +0200

    vari problemi su face di google

--- app/Http/Livewire/CreateAnnouncement.php
+++ app/Http/Livewire/CreateAnnouncement.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use App\Models\Announcement;
 use Livewire\WithFileUploads;
@@ -80,9 +81,12 @@ class CreateAnnouncement extends Component
                 $newFileName = "announcements/{$this->announcement->id}";
                 $newImage = $this->announcement->images()->create(['path' =>$image->store($newFileName, 'public')]);
 
-                dispatch(new ResizeImage($newImage->path , 800, 700));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                RemoveFaces::withChain([
+                    
+                    new ResizeImage($newImage->path , 800, 700),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
             
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
