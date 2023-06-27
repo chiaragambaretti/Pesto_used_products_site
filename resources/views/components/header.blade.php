4969302ed46245f3df78cf500cda5d89e59e3059
salvatore bonomo <salvatore.bonomo20@gmail.com>
2023-06-27 12:36:11 +0200

    presentazione

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid headerBox">
   <div class="row">
     <div class="col-12 textCustom">
-      <h1 class="header-h1 ">Presto Ecommerce</h1>
+      <h1 class="header-h1 ">Presto</h1>
       <p class="header-p">
-        {{__('ui.header')}} <br> {{__('ui.header2')}} <br> {{__('ui.header3')}}
-
+        {{-- {{__('ui.header')}}  --}}
+        {{__('ui.header3')}} <br> {{__('ui.header2')}}
       </p>
     </div>
   </div>
